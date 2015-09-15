769257dd9270e3815b368dd20c0c317641e1feb0
z1c0 <wziegler@live.at>
2015-09-15 13:44:15 +0200

    Create window app (Westmisnter)

---
+++ App/Ohm/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Ohm
+{
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+      edgeView.Navigate(new Uri("http://ohms.azurewebsites.net"));
+      SetEdgeSize();
+    }
+
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+      base.OnNavigatedTo(e);
+      Window.Current.SizeChanged += Current_SizeChanged;
+    }
+
+    private void Current_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+    {
+      SetEdgeSize();
+    }
+
+    private void SetEdgeSize()
+    {
+      edgeView.Width = Window.Current.Bounds.Width;
+      edgeView.Height = Window.Current.Bounds.Height;
+    }
+
+    protected override void OnNavigatedFrom(NavigationEventArgs e)
+    {
+      base.OnNavigatedFrom(e);
+      Window.Current.SizeChanged -= Current_SizeChanged;
+    }
+  }
+}
